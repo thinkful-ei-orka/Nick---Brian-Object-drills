2a282a292d537b326f378471e50058d6001ff5de
bcharles41078 <62114382+bcharles41078@users.noreply.github.com>
2020-04-21 15:58:26 -0500

    Add files via upload

--- objectDrills3.js
+++ objectDrills3.js
@@ -1,16 +1,24 @@
 const firstPerson = {
   name: "Jon",
-  jobTitle: "Night's Watch"
+  jobTitle: "Owner"
 }
 const secondPerson = {
   name: "Rob",
-  jobTitle: "King in the North"
+  jobTitle: " CFO"
 }
 const thirdPerson = {
   name: "Ned",
-  jobTitle: "dying"
+  jobTitle: "Accountant"
 }
 
 let myArray = [firstPerson, secondPerson, thirdPerson];
 
-myArray.forEach(element => console.log(element));
+//myArray.forEach(element => console.log(element));
+for (i=0;i<myArray.length;i++){
+  if (myArray[i].jobTitle === 'Owner'){
+    
+    console.log(myArray[i].jobTitle)
+    console.log (`${myArray[i].jobTitle} ${myArray[i].name} doesn't report to anybody.`);
+  }
+  console.log (`${myArray[i].jobTitle} ${myArray[i].name} reports to ${firstPerson.name}.`);
+}
